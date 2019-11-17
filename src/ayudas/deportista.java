e83aded710de23c608849e37fe8be11dbec3cbaa
Maynor21 <maynorpiotta@gmail.com>
2019-11-16 19:21:12 -0600

    agregando mi subclase deportista del trabajo grupal

---
+++ src/ayudas/deportista.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayudas;
+
+/**
+ *
+ * @author mayno
+ */
+public class deportista extends persona {
+    
+     public deportista(){
+        
+           setNombre("Cristiano Ronaldo ");
+          setEdad(30);
+           setTrabajo("Juntus");
+            setExperiencia(15);
+    }
+    
+}
